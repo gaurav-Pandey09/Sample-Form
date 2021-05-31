e2164eb1575f3d3f0efcea6947ff6d96987a15d3
Gaurav Pandey <gauravizhere180@gmail.com>
2021-05-31 13:59:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let button = document.getElementById("color-picker");
+
+defaultColor = () => {
+  button.style.backgroundColor = "rgba(231, 238, 237, 0.9)";
+};
+
+colorPink = () => {
+  button.style.backgroundColor = "rgba(230, 146, 205, 0.9)";
+};
+
+colorBlue = () => {
+  button.style.backgroundColor = "rgba(55, 66, 185, 0.9)";
+};
+
+colorPurple = () => {
+  button.style.backgroundColor = "rgba(177, 153, 242, 0.9)";
+};
+
+colorOrange = () => {
+  button.style.backgroundColor = "rgba(223, 119, 82, 0.9)";
+};
